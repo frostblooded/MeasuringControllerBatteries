05e3a3c70ed4867fe9918ddc2f671b927523fc1f
Nikolay Danailov <frostblooded@yahoo.com>
2016-06-06 10:01:29 +0300

    Some final changes

--- MeasuringControllerBatteries.ino
+++ MeasuringControllerBatteries.ino
@@ -1,12 +1,10 @@
 #include <avr/sleep.h>
-//TODO: Read from A0 the voltage of the battery(0 - 20.48V)
 
 #define VOLTAGE_PIN 0
 #define CURRENT_PIN 1
 #define CURRENT_ENABLE_PIN 19
 
 #define DEVICE_INDEX 0
-SoftwareSerial dataSerial(2, 3);
 int wakePin = 2;
 int sleepStatus = 0;
 
@@ -40,7 +38,7 @@ void wakeUpNow()
     res += "&battery_id=" + String(CURRENT_ENABLE_PIN);
   }
 
-  dataSerial.write(res);
+  Serial.write(res);
   sleep(100);
 }
 
@@ -48,7 +46,7 @@ void setup()
 {
   pinMode(CURRENT_ENABLE_PIN, OUTPUT);
   pinMode(wakePin, INPUT);
-  dataSerial.begin(9600);
+  Serial.begin(9600);
   attachInterrupt(0, wakeUpNow, CHANGE);
 }
  
